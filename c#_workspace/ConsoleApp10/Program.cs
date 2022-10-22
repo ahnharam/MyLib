d3d1bc5a1d0d3cbf887c017a8f2f369bd16e9c47
DESKTOP-F2465BN\M <km436@naver.com>
2022-10-22 18:25:59 +0900

    [C#] - 김민수 숙제

---
+++ c#_workspace/ConsoleApp10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bool
+{
+    class MainApp
+        {
+        static void Main(String[] args)
+            {
+                bool a = true;
+                bool b = false;
+
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+        }
+}
---
+++ c#_workspace/ConsoleApp25/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+namespace StringSearch
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good Morning";
+
+            WriteLine(greeting);
+            WriteLine();
+
+            // IndexOf()
+            WriteLine("IndexOf 'Good' : {0}", greeting.IndexOf("Good"));
+            WriteLine("IndexOf 'o' : {0}", greeting.IndexOf('o'));
+
+            // LastIndexOf()
+            WriteLine("LastIndexOf 'Good' : {0}", greeting.IndexOf("Good"));
+            WriteLine("LastIndexOf 'o' : {0}", greeting.LastIndexOf("o"));
+
+            // StartsWith()
+            WriteLine("StratsWith 'Good' : {0}", greeting.StartsWith("Good"));
+            WriteLine("StartsWith 'Morning' : {0}", greeting.StartsWith("Mirning"));
+
+            //EndsWith()
+            WriteLine("EndsWith 'Good' : {0}", greeting.EndsWith("Good"));
+            WriteLine("EndsWith 'Morning' : {0}", greeting.EndsWith("Morning"));
+
+            // Contains()
+            WriteLine("Contains 'Evenig' : {0}", greeting.Contains("Exening"));
+            WriteLine("Contains 'Morning' : {0}", greeting.Contains("Morning"));
+
+            // Replace()
+            WriteLine("Replaced 'Morning' with 'Evening': {0}",
+                greeting.Replace("Morning", "Evening"));
+        }
+    }
+}
