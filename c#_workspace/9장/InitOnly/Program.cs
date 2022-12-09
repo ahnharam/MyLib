27df9823f042428a9e0576cdc80f43b1b5cfa8aa
DESKTOP-F2465BN\M <km436@naver.com>
2022-12-09 17:34:34 +0900

    2022.12.09

---
+++ c#_workspace/9장/InitOnly/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace InitOnly
+{
+    class Transaction
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Amount { get; init; }
+
+        public override string ToString()
+        {
+            return $"{From,-10}->{To,-10} : ${Amount}";
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Transaction tr1 = new Transaction
+            {
+                From = "Alice",
+                To = "Bob",
+                Amount = 100
+            };
+            Transaction tr2 = new Transaction
+            {
+                From = "Bob",
+                To = "Charlice",
+                Amount = 50
+            };
+            Transaction tr3 = new Transaction
+            {
+                From="Charlie",
+                To="Alice",
+                Amount=50
+            };
+
+            Console.WriteLine(tr1);
+            Console.WriteLine(tr2);
+            Console.WriteLine(tr3);
+        }
+    }
+}
