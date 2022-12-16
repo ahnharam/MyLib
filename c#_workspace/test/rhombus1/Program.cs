52036abee7a89f6f684ddc247f04546f0f1514b1
DESKTOP-F2465BN\M <km436@naver.com>
2022-12-16 18:08:43 +0900

    2022.12.16

---
+++ c#_workspace/test/rhombus1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace rhombus1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자 입력 : ");
+            int num =int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = num; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i * 2 ; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = num-2; i >= 0; i--)
+            {
+                for (int j = num; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i * 2 + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
