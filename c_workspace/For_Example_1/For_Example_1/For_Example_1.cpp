f0a7d07a347ca2da710a38db63da8b879a399e20
DESKTOP-GM3TLAS\blues <ahr1025@naver.com>
2023-01-27 00:17:26 +0900

    [C언어] - 2023_01_27 - 반복문

---
+++ c_workspace/For_Example_1/For_Example_1/For_Example_1.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS    // scanf 보안 경고로 인한 컴파일 에러 방지
+#include <stdio.h>
+
+void main() {
+	// for 
+	int a = 1;
+	int i;
+
+	scanf("%d", &a);
+	// 데이터를 받을 때 높은 수 10
+
+	// 데이터가 0
+
+	// 초기화, 조건문, 증감식
+	for ( ; ; ) {
+		printf("a : %d\n", a);
+	}
+}
