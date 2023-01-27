a59666d412e2baca2f87d7acca032e81b7c29325
DESKTOP-V21DR4A\정다빈 <wjdekqls4002@naver.com>
2023-01-27 22:38:37 +0900

    [JAVA] - 2023.01.27 - 숙제 업로드

---
+++ java_workspace/FirstProject/src/IfEx22.java
@@ -0,0 +1,17 @@
+
+public class IfEx22 {
+
+	public static void main(String[] args) {
+		int su1 = 51;
+		String str;
+		
+		if(su1 >= 50)
+		str = "50이상";
+		else
+	    str = "50미만";
+		System.out.println(str+"입니다.");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_workspace/FirstProject/src/OperEx2.java
@@ -0,0 +1,14 @@
+
+public class OperEx2 {
+
+	public static void main(String[] args) {
+		short a,b;
+		a = b = 10;
+		
+		int c = a + b;
+		System.out.println(" c의 값 :" +c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
