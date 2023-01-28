7cfbb9054508b03a525da3913cb573ade0d70e0d
DESKTOP-F2465BN\M <km436@naver.com>
2023-01-28 21:32:52 +0900

    2023.1.28

---
+++ c#_workspace/7장/MethodHiding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodHiding
+{
+    class Base
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Base.MyMethod()");
+        }
+    }
+
+    class Derived : Base
+    {
+        public new void MyMothod()
+        {
+            Console.WriteLine("Derived.MyMethod()");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Base baseObj=new Base();
+            baseObj.MyMethod();
+
+            Derived derivedObj=new Derived();
+            derivedObj.MyMothod();
+
+            Base baseOrDerived = new Derived();
+            baseOrDerived.MyMethod();
+        }
+    }
+}
---
+++ c#_workspace/9장/WithExp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WithExp
+{
+    record RTransaction
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Amount { get; set; }
+
+        public override string ToString()
+        {
+            return $"{From,-10} -> {To,-10} : ${Amount}";
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            RTransaction tr1 = new RTransaction
+            {
+                From = "Alice",
+                To = "Bob",
+                Amount = 100
+            };
+            RTransaction tr2 = tr1 with
+            {
+                To = "Charlie"
+            };
+            RTransaction tr3 = tr2 with
+            {
+                From = "Dave",
+                Amount = 30
+            };
+
+            Console.WriteLine(tr1);
+            Console.WriteLine(tr2);
+            Console.WriteLine(tr3);
+        }
+    }
+}
