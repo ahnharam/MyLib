d9c5f11dd320b7061bd78e81d5abe6e7908e017e
DESKTOP-2C61BJ6\박성현 <simon2428@naver.com>
2023-01-29 21:11:07 +0900

    [c언어] - 2023-01-29 - 코딩숙제

---
+++ while exam/while exam/while exam.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+void main() {
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("$\n");
+	}
+	i = 0;
+	printf("\n");
+
+	int a = 1;
+	while (a < 1024) {
+		a = a * 2;
+		printf("$\n");
+	}
+	a = 0;
+	printf("\n");
+
+	int b = 1;
+	do
+	{
+		b = b * 2;
+		printf("$\n");
+	} while (b < 1024);
+
+
+}
