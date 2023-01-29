eeb1b660831878160a8eed8a952ae258ae31eee6
DESKTOP-GM3TLAS\blues <ahr1025@naver.com>
2023-01-29 22:28:28 +0900

    [C언어] - 2023_01_29 - 함수

---
+++ c_workspace/Function_Example_1/Function_Example_1/Function_Example_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+// 선언
+void print(int c);
+int sum(int x, int y);
+
+void main() {
+	// 함수 == function
+	int a = 10;
+	int b = 20;
+
+	// 호출
+	int c = sum(a, b);
+
+	// 호출
+	print(c);
+}
+
+// 정의
+void print(int x) {
+	printf("출력\n %d", x);
+}
+
+//			10		20
+// 정의
+int sum(int x, int y) {
+	printf("출력 %d\n", x);
+	printf("출력 %d\n", y);
+	printf("출력 %d\n", x + y);
+	//		30
+	return x + y;
+}
