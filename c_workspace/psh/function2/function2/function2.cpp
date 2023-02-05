789c940f31b4db49ecd94f7beeca9bb936ac200d
DESKTOP-2C61BJ6\박성현 <simon2428@naver.com>
2023-02-05 20:52:29 +0900

    [c언어] - 2023 - 02 - 05 - 함수예제, 확인문제

---
+++ c_workspace/psh/function2/function2/function2.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int sum(int x, int y);
+
+int main() {
+	int a = 10, b = 20;
+	int result;
+
+	result = sum(a, b);
+	printf("result :%d\n", result);
+	return 0;
+}
+int sum(int x, int y) {
+	int temp;
+	temp = x + y;
+	return temp;
+}
