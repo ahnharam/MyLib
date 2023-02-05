4657839a45a7ec2099bd08c2d8feb14100baa251
DESKTOP-GM3TLAS\blues <ahr1025@naver.com>
2023-02-05 22:32:01 +0900

    [c언어] - 2023-02-05 - 배열, 포인터

---
+++ c_workspace/ArrayExample2/ArrayExample2/ArrayExample2.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+	char str1[80] = "cat";
+
+	strcpy_s(str1, "tiger");
+
+	printf_s("%s", str1);
+
+	char str[10];
+	puts("입력된 문자열 : ");
+	gets_s(str);
+	puts(str);
+
+}
---
+++ c_workspace/PointExample1/PointExample1/PointExample1.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+/*
+	포인터 : 변수들을 통째로 옮기는 이동수단
+
+	구조체 : 변수의 집합
+*/
+void test(int* a);
+void array(int* a);
+
+void main() {
+
+
+	/*int a = 10;
+	int* b = &a;
+
+	*b = 20;
+	printf("%d", a);*/
+	
+	int a[10];
+	int* b = a;
+
+	*b = 10;
+	*(b + 1) = 20;
+
+	printf("%d", a[0]);
+	printf("%d", a[1]);
+}
+
+// a = 10
+void array(int* a) {
+	*a = 10;
+	printf("%d", *a);
+}
