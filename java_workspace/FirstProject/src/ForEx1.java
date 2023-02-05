420b230143bd55feeb637b488c3210495e4cf342
DESKTOP-V21DR4A\정다빈 <wjdekqls4002@naver.com>
2023-02-05 19:21:51 +0900

    [JAVA] - 2023.02.01 - 숙제 업로드

---
+++ java_workspace/FirstProject/src/ForEx1.java
@@ -0,0 +1,13 @@
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i <= 5 ; i++) {
+			System.out.println(i+"번째 수행");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
