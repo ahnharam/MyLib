9c719ebd3af4ebe194cc498ac73bcbff2a1f0d85
DESKTOP-2C61BJ6\박성현 <simon2428@naver.com>
2023-02-15 22:11:03 +0900

    [C언어 - 2023 _ 02 _ 15 - 배열

---
+++ c_workspace/psh/fuction2 02-08/fuction2 02-08/fuction2 02-08.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+void print_line();
+
+void main() {
+	print_line();
+	printf("학번     이름     전공    학점\n");
+	print_line();
+}
+
+void print_line(){
+	int i;
+	for (i = 0; i < 50; i++) {
+		printf("-");
+	}
+	printf("\n");
+}
---
+++ c_workspace/psh/function 204 02-09/function 204 02-09/function 204 02-09.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+void sum(int a);
+
+int main() {
+	sum(10);
+	sum(100);
+	return 0;
+}
+
+ void sum(int a) {
+	 int i, tot = 0;
+	 for (i = 1; i <= a; i++) {
+		 tot += i;
+		 
+	 }
+	 printf("1부터 %d까지의 합은 %d입니다.\n",a, tot);
+}
