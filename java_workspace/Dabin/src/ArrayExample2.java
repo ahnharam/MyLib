449983669bbeb677bcfdd7dfab3e51675cec3c2c
DESKTOP-GM3TLAS\blues <ahr1025@naver.com>
2023-02-28 01:45:44 +0900

    [JAVA] - 2023.02.28 - 배열

---
+++ java_workspace/Dabin/src/ArrayExample2.java
@@ -0,0 +1,13 @@
+
+public class ArrayExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassExample4 ex1[] = new ClassExample4[5];
+		ex1[0].a = 10;
+		ex1[0].b = 20;
+		
+		ex1[1].b = 40;
+	}
+
+}
---
+++ java_workspace/Dabin/src/ClassExample4.java
@@ -0,0 +1,10 @@
+
+public class ClassExample4 {
+	public int a = 10;
+	public int b = 20;
+	public String s = "hello";
+	
+	public int sum(int x, int y) {
+		return x + y;
+	}
+}
